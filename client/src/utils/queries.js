29dfdc92c7776317576935daee09657c1d26ce2b
sp381 <sarah.powell381@gmail.com>
2022-06-18 22:07:35 -0700

    created new files

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -56,3 +56,31 @@ export const QUERY_USER = gql`
     }
   }
 `;
+
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
